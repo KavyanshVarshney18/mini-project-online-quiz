e2cbcbd27adb1d62aa863b8f929bc8bee33cf74b
Satyam Varshney <sv1061211@gmail.com>
2026-04-28 14:46:14 +0530

    Add files via upload

---
+++ OpenApiConfig.java
@@ -0,0 +1,31 @@
+package com.quizapp.quiz_system.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI quizSystemOpenApi() {
+        final String securitySchemeName = "bearerAuth";
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Quiz System API")
+                        .version("1.0")
+                        .description("Role-based online quiz management and attempt API"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .schemaRequirement(
+                        securitySchemeName,
+                        new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                );
+    }
+}
